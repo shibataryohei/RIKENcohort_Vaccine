0db047c5fad3c6be6ca22ec8568a715bf0186c97
shibataryohei <shibataryohei@gmail.com>
2023-02-23 19:10:52 -0500

    Cohort compare

--- RIKENcohort-Vaccine_Setup.R
+++ RIKENcohort-Vaccine_Setup.R
@@ -6,9 +6,24 @@ source("~/Git/Database/Library/Library_Basic.R" )
 source("~/Git/Database/Library/Library_Bioinformatics.R")
 source("~/Git/Database/Function/Function.R")
 source("~/Git/Database/Function/Function_GE.R")
-source("~/Git/Database/RIKENcohort/RIKENcohort_Function.R")
 
+source("~/Git/Database/RIKENcohort/RIKENcohort_Function.R")
 source("~/Git/Database/RIKENcohort/RIKENcohort_Skin.R")
 
 source("~/Git/Database/RIKENcohort/RIKENcohort_Vaccine.R")
-source("~/Git/Database/RKcohort/RKcohort_Clinicaldata.R")
+
+# Clinicaldta_tbw
+source("~/Git/Database/RIKENcohort/RIKENcohort_Clinicaldata.R")
+
+Cohort_tbw %>% 
+  inner_join(Antibiotics_tbw) %>% 
+  inner_join(Smoking_tbw) %>% 
+  inner_join(Parents_tbw) %>% 
+  inner_join(Perinatal_tbw) %>% 
+  inner_join(SolidFood_tbw) %>% 
+  inner_join(HBM_tbw) %>% 
+  inner_join(Sibling_Pet_tbw) %>% 
+  inner_join(Daycare_tbw) -> Clinicaldata_tbw
+  
+  
+  
